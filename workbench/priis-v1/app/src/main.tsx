96d396e170a6207192f4b8dbb2b8abce4c763d2b
jotaele44 <jorge.luis1352@gmail.com>
2026-07-10 14:47:56 -0400

    feat(ui): activate SpiderWeb federation theme identity

--- workbench/priis-v1/app/src/main.tsx
+++ workbench/priis-v1/app/src/main.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "maplibre-gl/dist/maplibre-gl.css";
+import "./styles/federation.css";
 import "./styles/app.css";
 import App from "./App";
+
+document.documentElement.dataset.repo = "spiderweb-pr";
+document.documentElement.dataset.theme = "dark";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
