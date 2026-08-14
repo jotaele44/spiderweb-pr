10c28a8303302cc75f085c48ad1e9abc6a1e408b
jotaele44 <jorge.luis1352@gmail.com>
2026-08-14 14:25:33 -0400

    Type Martin frontend environment variable

--- server/frontend/src/vite-env.d.ts
+++ server/frontend/src/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE?: string;
+  readonly VITE_MARTIN_BASE?: string;
   readonly VITE_TILE_URL?: string;
   readonly VITE_TILE_ATTRIBUTION?: string;
 }
